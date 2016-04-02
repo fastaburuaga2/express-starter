9ca4b5a591d331a43c2bb943fa02b3081e082883
fastaburuaga22 <fastaburuaga@uc.cl>
2016-04-02 20:50:24 -0300

    Methods POST and GET working without MongoDB yet

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const express = require('express')
 const morgan = require('morgan')
 
@@ -7,6 +9,26 @@ app.use(morgan('tiny'))
 app.get('/', (req, res) => {
   // Send JSON
   res.send({ status: 'on' })
+})
+
+let bears = []
+let index = 0
+
+//EXAMPLE: POST http://localhost:3000/bears?bear1=juan&bear2=jose
+app.post('/bears', (req, res) => {
+  // Send JSON
+  for(var key in req.query){
+    bears[index] = req.query[key]
+    index++
+  }
+
+  res.send(`Your bears are ${bears}`)
+})
+
+app.get('/bears', (req, res) => {
+  // Send JSON
+  console.log(req.query)
+  res.send({ bears })
 })
 
 module.exports = app
